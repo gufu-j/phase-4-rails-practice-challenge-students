d9920604d9eb714e2c5f14ae01d3b9fe95484792
diego <guatemaladiegojose@gmail.com>
2023-08-18 18:27:15 -0400

    added show and render not found response

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,8 +1,27 @@
 class InstructorsController < ApplicationController
 
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def index 
         instructors = Instructor.all
         render json: instructors
+    end
+
+    def show
+        instructor = Instructor.find(params[:id])
+        render json: instructor
+    end
+
+    def create 
+
+    end
+    
+
+
+    private
+
+    def render_not_found_response 
+        render json: { error: "Instructor not found"}, status: :not_found 
     end
 
 end
