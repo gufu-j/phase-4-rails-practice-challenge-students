4b6e55718e6e06f7182137cd7b0239f97a2238f1
diego <guatemaladiegojose@gmail.com>
2023-08-18 19:16:19 -0400

    added destroy method to delete Instructors after considering routing and controller setup

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -2,6 +2,8 @@ class InstructorsController < ApplicationController
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
     def index 
         instructors = Instructor.all
         render json: instructors
@@ -13,15 +15,39 @@ class InstructorsController < ApplicationController
     end
 
     def create 
-
+        instructor = Instructor.create!(instructor_params)
+        render json: instructor, status: :created
     end
-    
+
+    def destroy 
+        instructor = find_instructor
+        instructor.destroy
+        head :no_content
+    end
+
+
+
 
 
     private
 
+    def find_instructor
+        Instructor.find(params[:id])
+    end
+
+    def instructor_params
+        params.permit(:name)
+    end
+
     def render_not_found_response 
         render json: { error: "Instructor not found"}, status: :not_found 
     end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    
+
 
 end
